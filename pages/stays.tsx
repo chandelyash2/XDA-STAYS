e111af00eb57a2fb95d83f9c054358b83d877f39
codeyash02 <yash.codedrill@gmail.com>
2022-11-14 18:50:29 +0530

    add stays page

--- pages/stays.tsx
+++ pages/stays.tsx
@@ -1,59 +1,58 @@
-import React from 'react'
-import Container from '../components/Container'
-import Layout from '../components/Layout'
-
+import React from 'react';
+import Container from '../components/Container';
+import Layout from '../components/Layout';
 
 const Stays = () => {
   const array = [
     {
-      index: 1
+      index: 1,
+      url: 'https://img.cdn.zostel.com/zostel/gallery/images/UckUVWBuRO26Pzrm9kNB8A/chamba-20220602201924.jpg?h=400',
+      name: 'Chamba',
     },
     {
-      index: 2
+      index: 2,
+      url: 'https://img.cdn.zostel.com/zostel/gallery/images/Au2EbwKgSbm7VRBzFhzwIg/taste-adventure-in-the-himalayas-of-bir-202_H7vz0yf.jpg?h=400',
+      name: 'Bir',
     },
     {
-      index: 3
+      index: 3,
+      url: "https://img.cdn.zostel.com/zostel/gallery/images/AogyCX0KQpeNYeOK545cgA/burwa-20220520080419.jpg?h=400",
+      name: "Burwa"
     },
     {
-      index: 4
+      index: 4,
+      url: "https://img.cdn.zostel.com/zostel/gallery/images/_7osfwtmT9SiCNJo_Z5LGw/jibhi-20220129082028.jpeg?h=400",
+      name: "Jibhi"
     },
     {
-      index: 5
+      index: 5,
+      url: "https://img.cdn.zostel.com/zostel/gallery/images/Rp7QpX-JS42R933SRiONNw/dharamkot-20220807174126.jpg?h=400",
+      name: "Dharamkot"
     },
-    {
-      index: 6
-    },
-    {
-      index: 7
-    },
-    {
-      index: 8
-    },
-    {
-      index: 9
-    }
-  ]
+
+  ];
   return (
     <Layout>
-      <div className='my-32 '>
+      <div className="my-32 ">
         <Container>
-          <div className='text-center text-2xl font-bold'>Explore <span className='text-[#f15824]'>Stays</span></div>
-          <div className='grid grid-cols-2 gap-4 text-center lg:grid-cols-4 gap-4 text-center ' >
-            {
-              array.map((d, i) => (
-                <div key={i} className='w-30 h-48 border border-indigo-600 my-10 justify-center lg:w-56'>
-                  {d?.index}
-                </div>
-              ))
-            }
-
-
-
+          <div className="text-center text-2xl font-bold">
+            Explore <span className="text-[#f15824]">Stays</span>
+          </div>
+          <div className="flex flex-wrap justify-around gap-4 text-center ">
+            {array.map((d, i) => (
+              <div
+                key={i}
+                className="flex font-bold text-white cursor-pointer items-center bg-cover w-36 h-48 border rounded-md my-10 justify-center lg:w-72 "
+                style={{ backgroundImage: `url(${d.url})` }}
+              >
+                <span> {d?.name}</span>{' '}
+              </div>
+            ))}
           </div>
         </Container>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Stays
+export default Stays;
